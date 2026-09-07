3a0f8eca91f550853ad801d43136eccc3a45f2b7
alogunov27-design <alogunov27@gmail.com>
2026-09-07 13:32:39 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+window.FNWP_CONFIG = {
+  supabaseUrl: "https://qskgabximepazaabckae.supabase.co",
+  supabaseAnonKey: "sb_publishable_KZN8uKD_OdqkPp8E38EnFA_luXbeqgy",
+  reservedNicks: ["fuckfaz", "admin", "administrator", "dev", "owner"],
+  donateLinks: {
+    300: "",
+    500: "",
+    1000: "",
+  },
+};
