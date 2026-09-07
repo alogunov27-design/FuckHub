2cf344a0f9081df97832b0652e92e590221d02e1
alogunov27-design <alogunov27@gmail.com>
2026-09-07 13:26:36 +0700

    config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.FNWP_CONFIG = {
   supabaseUrl: "https://qskgabximepazaabckae.supabase.co",
-  supabaseAnonKey: "sb_publishable_KZN8uKD_OdqkPp8E38EnFA_luXbeqgy",
+  supabaseAnonKey: "sb_publishable_KZN8uKD_0dqkPp8E38EnFA_luXbeqgy",
   reservedNicks: ["fuckfaz", "admin", "administrator", "dev", "owner"],
   donateLinks: {
     300: "",
