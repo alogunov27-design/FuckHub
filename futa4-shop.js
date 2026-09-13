121af39470f7dea04876f647916ffe8bd8a5153b
alogunov27-design <alogunov27@gmail.com>
2026-09-13 11:07:35 +0700

    Add futa4 member girl ids

--- futa4-shop.js
+++ futa4-shop.js
@@ -1,14 +1,31 @@
 (function () {
+  window.FUTA4_GIRLS = ["fexa_chlen", "frenni_chlen", "bonfie_chlen", "chiku_chlen"];
+  var packImg = "./img/IMG_20260913_071058_397.jpg";
+  var extras = [
+    { id: "futa4", name: "Бабы с яйцами", img: packImg },
+    { id: "fexa_chlen", name: "Fexa_chlen", img: packImg },
+    { id: "frenni_chlen", name: "Frenni_chlen", img: packImg },
+    { id: "bonfie_chlen", name: "Bonfie_chlen", img: packImg },
+    { id: "chiku_chlen", name: "Chiku_chlen", img: packImg }
+  ];
   if (typeof CATALOG !== "undefined") {
-    if (!CATALOG.some(function (x) { return x.id === "futa4"; })) {
-      CATALOG.push({
-        id: "futa4",
-        name: "Бабы с яйцами",
-        img: "./img/IMG_20260913_071058_397.jpg"
-      });
-    }
+    extras.forEach(function (g) {
+      if (!CATALOG.some(function (x) { return x.id === g.id; })) CATALOG.push(g);
+    });
   }
   if (typeof RUB_PRICE !== "undefined") RUB_PRICE.futa4 = 400;
+
+  var _mine = window.renderMine;
+  if (typeof _mine === "function") {
+    window.renderMine = function () {
+      var hide = { fexa_chlen: 1, frenni_chlen: 1, bonfie_chlen: 1, chiku_chlen: 1 };
+      var old = owned && owned.slice();
+      if (Array.isArray(owned)) owned = owned.filter(function (id) { return !hide[id]; });
+      var r = _mine.apply(this, arguments);
+      if (old) owned = old;
+      return r;
+    };
+  }
 
   function strike() {
     document.querySelectorAll(".girl-card, .inv-card").forEach(function (card) {
@@ -24,7 +41,6 @@
       }
     });
   }
-
   function wrap(name) {
     var fn = window[name];
     if (typeof fn !== "function") return;
